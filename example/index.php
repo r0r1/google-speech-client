a81303cf3ae2b3e53fcf9762b97c2377e0575ca2
Rori Kurniadi <rori.kurniadi@yahoo.com>
2017-05-02 02:05:21 +0700

    remove unused file

--- example/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-require __DIR__ . '/../vendor/autoload.php';
-
-$fileName = __DIR__ . '/../public/big.wav';
-$fileName = fopen($fileName, 'r');
-
-$upload = new Rorikurn\GoogleSpeechClient\Upload($fileName);
-$upload->process($fileName);
